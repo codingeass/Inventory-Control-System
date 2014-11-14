8c91592899d417be73593c04c408f04a61d88113
codingeass <amandeeptheviper@gmail.com>
2014-11-14 19:18:41 +0530

    all frames of inventory control system added with jar

---
+++ src/project/ProjectPractical.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package project;
+
+/**
+ *
+ * @author amandeep gupta
+ */
+public class ProjectPractical {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
